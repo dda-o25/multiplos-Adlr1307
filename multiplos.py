b47165ba4a847e90d41172ddaf586af2d9ad6731
Adlr1307 <andres.jaime@iteso.mx>
2025-09-18 16:51:35 -0600

    Solución a multiplos de 0

--- multiplos.py
+++ multiplos.py
@@ -5,8 +5,12 @@ Andrés Enrique Jaime de la Rosa, 763799
 na=int(input("Introduzca un número "))
 nb=int(input("Introduzca otro número "))
 # Proceso
-if na == 0 or nb == 0:
-    print("Introduzca un numero diferente a 0")
+if na == 0 and nb == 0:
+    print("Los dos números no pueden ser 0")
+elif na == 0:
+    print("El número",na,"es múltiplo del",nb)
+elif nb == 0:
+    print("El número",nb,"es múltiplo del",na)
 else:    
     multiplo = na%nb
     multiplo2 = nb%na
@@ -16,4 +20,7 @@ else:
         print("El número",nb,"es múltiplo del",na)
     else:
         print("Ninguno de los números es múltiplo del otro")
+
+
+
 # Salidas
