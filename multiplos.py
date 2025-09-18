85fee4bf9905219b486a1cf923b586ca4ccb12e2
Adlr1307 <andres.jaime@iteso.mx>
2025-09-17 19:49:49 -0600

    Solución para division a 0 y correcciones

--- multiplos.py
+++ multiplos.py
@@ -5,12 +5,15 @@ Andrés Enrique Jaime de la Rosa, 763799
 na=int(input("Introduzca un número "))
 nb=int(input("Introduzca otro número "))
 # Proceso
-multiplo = na%nb
-multiplo2 = nb%na
-if multiplo // 1 == 0:
-    print("El número",na,"es multiplo de",nb)
-elif multiplo2 // 1 == 0:
-    print("El número",nb,"es multiplo de",na)
-else:
-    print("Ninguno de los números es múltiplo del otro")
+if na == 0 or nb == 0:
+    print("Introduzca un numero diferente a 0")
+else:    
+    multiplo = na%nb
+    multiplo2 = nb%na
+    if multiplo == 0:
+        print("El número",na,"es múltiplo del",nb)
+    elif multiplo2 == 0:
+        print("El número",nb,"es múltiplo del",na)
+    else:
+        print("Ninguno de los números es múltiplo del otro")
 # Salidas
