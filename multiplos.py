af06631c1f6838d116ce97a99091e11f09ef9d4c
Adlr1307 <andres.jaime@iteso.mx>
2025-09-17 19:25:12 -0600

    Código inicial

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,16 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Andrés Enrique Jaime de la Rosa, 763799
 """
-
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
-
+na=int(input("Introduzca un número "))
+nb=int(input("Introduzca otro número "))
 # Proceso
-
-
+multiplo = na%nb
+multiplo2 = nb%na
+if multiplo // 1 == 0:
+    print("El número",na,"es multiplo de",nb)
+elif multiplo2 // 1 == 0:
+    print("El número",nb,"es multiplo de",na)
+else:
+    print("Ninguno de los números es múltiplo del otro")
 # Salidas
-print(salida)
